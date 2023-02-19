b92156a2c44cbefbf501e632e6c1d09904aaaf53
Mathieu Guindon <mathieu.guindon@live.ca>
2023-02-18 21:49:18 -0500

    more cleanup

--- Rubberduck.RPC/Platform/Model/ServerState.cs
+++ Rubberduck.RPC/Platform/Model/ServerState.cs
@@ -6,36 +6,6 @@ using System.Text.Json.Serialization;
 
 namespace Rubberduck.RPC.Platform.Model
 {
-    /// <summary>
-    /// Describes the current state of a RPC server.
-    /// </summary>
-    public enum ServerStatus
-    {
-        /// <summary>
-        /// The initial server state.
-        /// </summary>
-        Starting,
-        /// <summary>
-        /// The server is configured and listening, but no client has connected yet.
-        /// </summary>
-        Started,
-        /// <summary>
-        /// Received an <c>Initialize</c> request, awaiting client <c>Initialized</c> notification.
-        /// </summary>
-        AwaitingInitialized,
-        /// <summary>
-        /// Client has sent an <c>Initialized</c> request, signaling it is ready to send requests and accept notifications.
-        /// </summary>
-        Initialized,
-        /// <summary>
-        /// Server is shutting down.
-        /// </summary>
-        ShuttingDown,
-        /// <summary>
-        /// Server process is terminating.
-        /// </summary>
-        Terminating,
-    }
 
     /// <summary>
     /// Holds the immutable base server state.
@@ -68,16 +38,9 @@ namespace Rubberduck.RPC.Platform.Model
     /// </remarks>
     public class BasicServerInfo : IServerInfo
     {
-        [JsonPropertyName("processId")]
         public int ProcessId { get; set; }
-
-        [JsonPropertyName("name")]
         public string Name { get; set; }
-
-        [JsonPropertyName("version")]
         public string Version { get; set; }
-
-        [JsonPropertyName("startTime")]
         public DateTime? StartTime { get; set; }
     }
 
---
+++ Rubberduck.RPC/Platform/Model/ServerStatus.cs
@@ -0,0 +1,33 @@
+﻿namespace Rubberduck.RPC.Platform.Model
+{
+    /// <summary>
+    /// Describes the current state of a RPC server.
+    /// </summary>
+    public enum ServerStatus
+    {
+        /// <summary>
+        /// The initial server state.
+        /// </summary>
+        Starting,
+        /// <summary>
+        /// The server is configured and listening, but no client has connected yet.
+        /// </summary>
+        Started,
+        /// <summary>
+        /// Received an <c>Initialize</c> request, awaiting client <c>Initialized</c> notification.
+        /// </summary>
+        AwaitingInitialized,
+        /// <summary>
+        /// Client has sent an <c>Initialized</c> request, signaling it is ready to send requests and accept notifications.
+        /// </summary>
+        Initialized,
+        /// <summary>
+        /// Server is shutting down.
+        /// </summary>
+        ShuttingDown,
+        /// <summary>
+        /// Server process is terminating.
+        /// </summary>
+        Terminating,
+    }
+}
