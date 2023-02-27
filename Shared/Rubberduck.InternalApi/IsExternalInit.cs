f5e0ccbe01c9f78a98d33e21b22b3cae5a185039
mansellan <mansellan@gmail.com>
2023-02-27 22:10:05 +0000

    Adjusted file system layout to match solution

---
+++ Shared/Rubberduck.InternalApi/IsExternalInit.cs
@@ -0,0 +1,14 @@
+﻿namespace System.Runtime.CompilerServices
+{
+    using System.ComponentModel;
+    /// <summary>
+    /// Reserved to be used by the compiler for tracking metadata.
+    /// This class should not be used by developers in source code.
+    /// </summary>
+    
+    // TODO: Remove this class if we move to net6 (it's only needed for net framework)
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    internal static class IsExternalInit
+    {
+    }
+}
