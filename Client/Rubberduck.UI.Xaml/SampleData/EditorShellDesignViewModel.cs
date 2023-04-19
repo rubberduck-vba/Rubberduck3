b5515dec0202c55c064e35910f99adf4d3904ab4
Tom Helm <tomhelm@gmail.com>
2023-04-19 15:34:14 +0100

    Partial working editor shell and status bar design data

---
+++ Client/Rubberduck.UI.Xaml/SampleData/EditorShellDesignViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Rubberduck.UI.Xaml.Controls
+{
+    internal class EditorShellDesignViewModel
+    {
+        public ObservableCollection<CodePaneDesignViewModel> ModuleDocumentTabs { get; set; } = new ObservableCollection<CodePaneDesignViewModel>
+        {
+            new CodePaneDesignViewModel { ModuleInfo = new ModuleInfoDesignViewModel { Name = "Module1" }},
+            new CodePaneDesignViewModel { ModuleInfo = new ModuleInfoDesignViewModel { Name = "Module2" }}
+        };
+
+        public CodePaneDesignViewModel SelectedModuleDocumentTab { get; set; } = new CodePaneDesignViewModel();
+
+        public IEnumerable<ShellToolTabDesignViewModel> ToolTabs { get; set; } = new ObservableCollection<ShellToolTabDesignViewModel>
+        {
+            new ShellToolTabDesignViewModel { Name = "Tooltab 1"},
+            new ShellToolTabDesignViewModel { Name = "Tooltab 2"}
+        };
+
+        public ShellToolTabDesignViewModel SelectedToolTab { get; set; } = new ShellToolTabDesignViewModel();
+
+        public StatusBarDesignViewModel Status { get; set; } = new StatusBarDesignViewModel();
+
+    }
+
+    internal class CodePaneDesignViewModel
+    {
+        public ModuleInfoDesignViewModel ModuleInfo { get; set; } = new ModuleInfoDesignViewModel();
+    }
+
+    internal class ModuleInfoDesignViewModel
+    {
+        public object QualifiedModuleName { get; set; } = new object();
+        public string Name { get; set; } = "Module1";
+    }
+
+    internal class ShellToolTabDesignViewModel
+    {
+        public string Name { get; set; } = "ToolTab1";
+        public object ViewModel { get; set; } = new object();
+        public ShellToolTabSettingDesignViewModel Settings { get; set; } = new ShellToolTabSettingDesignViewModel();
+    }
+
+    internal class ShellToolTabSettingDesignViewModel
+    {
+        public bool IsVisible { get; set; } = true;
+    }
+}
---
+++ Client/Rubberduck.UI.Xaml/SampleData/StatusBarDesignViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Rubberduck.UI.Xaml.Controls
+{
+    internal class StatusBarDesignViewModel
+    {
+        public int DocumentLines { get; set; } = 100;
+        public int DocumentLength { get; set; } = 1000;
+        public int CaretOffset { get; set; } = 5;
+        public int CaretLine { get; set; } = 1;
+        public int CaretColumn { get; set; } = 5;
+        public int IssuesCount { get; set; } = 3;
+        public string ParserState { get; set; } = "Ready";
+        public int ProgressValue { get; set; } = 50;
+        public int ProgressMaxValue { get; set; } = 100;
+        public bool ShowDocumentStatusItems { get; set; } = true;
+        public string CodeLocation { get; set; } = "Module1";
+    }
+}
