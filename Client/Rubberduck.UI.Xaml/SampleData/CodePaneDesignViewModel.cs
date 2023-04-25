b62616f407fa8057f01003d32ae865a84abc2152
Tom Helm <tomhelm@gmail.com>
2023-04-25 12:19:20 +0100

    Adding design data to editor (#62)
    Sets up fake data for the XAML designer so that it is easier to see what
    the editor window will look like when working with it. Should have no
    impact on the runtime functionality as it just provides a data context
    for the designer.
    
    - EditorShellControl.xaml now uses new EditorShellDesignViewModel
    instead of IEditorShellViewModel
    - RubberduckEditorControl.xaml now uses CodePaneDesignViewModel instead
    of ICodePaneViewModel
    - StatusBarControl.xaml now uses StatusBarDesignViewModel instead of
    IStatusBarViewModel
    - SyncPanelToolControl.xaml now uses SyncPanelDesignViewModel instead of
    ISyncPanelToolTab
    
    Each of the 4 controls have data for viewing and all come through to
    EditorShellControl.xaml to look like this in VS:
    
    <img width="803" alt="EditorDesignView"
    src="https://user-images.githubusercontent.com/4892984/233693369-8dae4eab-2638-4a9c-8223-da904a2d5739.png">
    
    I don't know if this is worth having at this stage in the project but I
    found it a nice learning exercise anyway.

---
+++ Client/Rubberduck.UI.Xaml/SampleData/CodePaneDesignViewModel.cs
@@ -0,0 +1,107 @@
+﻿using ICSharpCode.AvalonEdit.Document;
+using Rubberduck.InternalApi.Model;
+using Rubberduck.UI.Abstract;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+using System;
+using Rubberduck.VBEditor;
+using System.Linq;
+
+namespace Rubberduck.UI.Xaml.Controls
+{
+    internal class CodePaneDesignViewModel : ICodePaneViewModel
+    {
+        public IModuleInfoViewModel ModuleInfo { get; set; } = new ModuleInfoDesignViewModel();
+        public ObservableCollection<IMemberProviderViewModel> MemberProviders { get; set; } = new ObservableCollection<IMemberProviderViewModel>
+        {
+            new MemberProviderDesignViewModel { Name = "Module1" },
+            new MemberProviderDesignViewModel { Name = "Class1" }
+        };
+        public IMemberProviderViewModel SelectedMemberProvider { 
+            get => MemberProviders.FirstOrDefault();
+            set { }
+        }
+        public string Content { get; set; } = "Sub MySub()\n    Debug.Print \"Hello World\"\nEnd Sub";
+        public IEditorSettings EditorSettings { get; set; } = new EditorSettingsDesignViewModel();
+
+        public TextDocument Document { get; set; }
+        public IEnumerable<ISyntaxErrorViewModel> SyntaxErrors => throw new NotImplementedException();
+        public IStatusBarViewModel Status => new StatusBarDesignViewModel();
+        public ICommand CloseCommand => new DummyCommand(); // => throw new NotImplementedException();
+
+        public event EventHandler SelectedMemberProviderChanged;
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    internal class DummyCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter) => true;
+
+        public void Execute(object parameter) { }
+    }
+
+    internal class ModuleInfoDesignViewModel : IModuleInfoViewModel
+    {
+        public IQualifiedModuleName QualifiedModuleName { get; set; } = new QualifiedModuleName("projectName", "projectPath", "componentName");
+        public string Name { get; set; } = "Module1";
+        public ModuleType ModuleType { get; set; } = ModuleType.StandardModule;
+        public string Folder { get; set; } = "VBAProject";
+        public Selection EditorPosition { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    internal class MemberProviderDesignViewModel : IMemberProviderViewModel
+    {
+        public string Name { get; set; } = "Module1";
+        public ModuleType ModuleType { get; set; } = ModuleType.StandardModule;
+        public IMemberInfoViewModel CurrentMember
+        {
+            get => Members.FirstOrDefault();
+            set { }
+        }
+        public ICollection<IMemberInfoViewModel> Members { get; set; } = new Collection<IMemberInfoViewModel>
+        {
+            new MemberInfoDesignViewModel { Name = "MySub" },
+            new MemberInfoDesignViewModel { Name = "Other" }
+        };
+        public IQualifiedModuleName QualifiedModuleName { get; set; } = new QualifiedModuleName("projectName", "projectPath", "componentName");
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void AddMember(string name, MemberType memberType, DocumentOffset offset)
+        {
+            throw new NotImplementedException();
+        }
+        public void ClearMemberSelectedHandlers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class MemberInfoDesignViewModel : IMemberInfoViewModel
+    {
+        public string Name { get; set; } = "MySub";
+        public MemberType MemberType { get; set; } = MemberType.Procedure;
+        public bool HasImplementation { get; set; }
+        public bool IsUserDefined { get; set; }
+        public string Signature { get; set; } = "";
+        public IParameterInfoViewModel CurrentParameter { get; set; }
+        public ObservableCollection<IParameterInfoViewModel> Parameters => throw new NotImplementedException();
+        public DocumentOffset Offset { get; set; }
+        public int StartLine { get; set; }
+        public int EndLine { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    internal class EditorSettingsDesignViewModel : IEditorSettings
+    {
+        public string FontFamily { get; set; } = "Arial";
+        public string FontSize { get; set; } = "14";
+        public bool ShowLineNumbers { get; set; } = false;
+        public double IdleTimeoutSeconds { get; set; } = 99;
+    }
+}
---
+++ Client/Rubberduck.UI.Xaml/SampleData/SyncPanelDesignViewModel.cs
@@ -0,0 +1,55 @@
+﻿using Rubberduck.InternalApi.Model;
+using Rubberduck.UI.Abstract;
+using Rubberduck.VBEditor;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Input;
+
+namespace Rubberduck.UI.Xaml.Controls
+{
+    internal class SyncPanelDesignViewModel : ISyncPanelViewModel
+    {
+        public ObservableCollection<ISyncPanelModuleViewModel> VBIDEModules => new ObservableCollection<ISyncPanelModuleViewModel>
+        {
+             new SyncPanelModuleDesignViewModel
+            {
+                ModuleType = ModuleType.StandardModule,
+                QualifiedModuleName = new QualifiedModuleName("projectName", "projectPath", "Module1")
+            },
+            new SyncPanelModuleDesignViewModel
+            {
+                ModuleType = ModuleType.ClassModule,
+                QualifiedModuleName = new QualifiedModuleName("projectName", "projectPath", "Class1")
+            }
+        };
+
+        public ISyncPanelModuleViewModel SelectedVBIDEModule
+        {
+            get => VBIDEModules.FirstOrDefault();
+            set { }
+        }
+
+        public IEditorShellViewModel Shell { get; set; }
+
+        public ICommand ReloadCommand => new DummyCommand();
+        public ICommand SyncCommand => new DummyCommand();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    internal class SyncPanelModuleDesignViewModel : ISyncPanelModuleViewModel
+    {
+        public IQualifiedModuleName QualifiedModuleName { get; set; } = new QualifiedModuleName("projectName", "projectPath", "Module1");
+        public ModuleType ModuleType { get; set; } = ModuleType.StandardModule;
+        public ModuleSyncState State { get; set; } = ModuleSyncState.OK;
+
+        public ICommand SyncCommand => new DummyCommand();
+        public ICommand LoadCommand => new DummyCommand();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
+
+
