ffbbf12e5d55a0ea00e818b1ce0e8544da46105b
Brian Zenger <bzngr@outlook.com>
2023-05-28 21:56:40 -0700

    Initial Implementation

---
+++ Client/Rubberduck.Client/LSP/WorkspaceService.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using System.IO.Abstractions;
+
+namespace Rubberduck.Client.LSP
+{
+    public class WorkspaceService
+    {
+        /// <summary>
+        /// Returns an array of (string, Folderpath) tuples used
+        /// to create/initialize LSP WorkspaceFolder objects
+        /// </summary>
+        public static (string Name, string Folderpath)[] GetWorkspaceFolderTuples(
+            IWorkspaceInfo wsInfo)
+        {
+            return new (string Name, string Folderpath)[]
+            {
+                ($"{wsInfo.HostDocumentName}(Saved)",
+                    wsInfo.SavedRepoInfo.FullName),
+                ($"{wsInfo.HostDocumentName}(Working)",
+                    wsInfo.WorkingRepoInfo.FullName)
+            };
+        }
+
+        /// <summary>
+        /// Creates the .rd folder structure for a given host document.
+        /// If the .rd folder and path(s) already exist, the function does nothing.
+        /// </summary>
+        public static void SetupDotRdFolder(IWorkspaceInfo wsInfo)
+        {
+            if (!wsInfo.WorkingRepoInfo.Exists)
+            {
+                wsInfo.WorkingRepoInfo.Create();
+            }
+
+            if (!wsInfo.SavedRepoInfo.Exists)
+            {
+                wsInfo.SavedRepoInfo.Create();
+            }
+
+            if (wsInfo.DotRdInfo.Attributes != FileAttributes.Hidden)
+            {
+                wsInfo.DotRdInfo.Attributes = FileAttributes.Hidden;
+            }
+        }
+    }
+}
---
+++ Tests/Rubberduck.Tests/Workspace/SetupDotRdFolderTests.cs
@@ -0,0 +1,78 @@
+using Moq;
+using System.IO.Abstractions;
+//using Rubberduck.Server.Workspace;
+using Rubberduck.Tests.Workspace;
+using Rubberduck.Client.LSP;
+
+namespace Rubberduck.Tests.WorkspaceServiceTests
+{
+    public class SetupDotRdFolderTests
+    { 
+        [Theory]
+        [InlineData(false, false, 1, 1)]
+        [InlineData(true, true, 0, 0)]
+        [InlineData(false, true, 1, 0)]
+        [InlineData(true, false, 0, 1)]
+        public void SetupDotRdFolder_WorkspaceFolderDoesNotExist_CreatesFolders(
+            bool workingExists, bool savedExists,
+            int expectedCreateWorking, int expectedCreateSaved)
+        {
+            //Arrange
+            var hostDoc = @"C:\users\duck\MyProjects\TestExcel\TestExcel.xlsm";
+
+            var mockWorkspaceInfo 
+                = WorkspaceTestsSupport.GetWorkspaceInfoFake(hostDoc);
+            Mock.Get(mockWorkspaceInfo.WorkingRepoInfo).Setup<bool>(di => di.Exists)
+                .Returns(workingExists);
+            Mock.Get(mockWorkspaceInfo.SavedRepoInfo).Setup<bool>(di => di.Exists)
+                .Returns(savedExists);
+
+            //Act
+            WorkspaceService.SetupDotRdFolder(mockWorkspaceInfo);
+
+            //Assert
+            Mock.Get(mockWorkspaceInfo.WorkingRepoInfo).Verify(wr => wr.Create(), 
+                Times.Exactly(expectedCreateWorking));
+
+            Mock.Get(mockWorkspaceInfo.SavedRepoInfo).Verify(wr => wr.Create(), 
+                Times.Exactly(expectedCreateSaved));
+        }
+
+        [Fact]
+        public void SetupDotRdFolder_WorkspaceFolderExists()
+        {
+            //Arrange
+            var hostDoc = @"C:\users\duck\MyProjects\TestExcel\TestExcel.xlsm";
+
+            var mockWorkspaceInfo
+                = WorkspaceTestsSupport.GetWorkspaceInfoFake(hostDoc, true);
+
+            //Act
+            WorkspaceService.SetupDotRdFolder(mockWorkspaceInfo);
+
+            //Assert
+            Mock.Get(mockWorkspaceInfo.WorkingRepoInfo)
+                .Verify(wri => wri.Create(), Times.Never);
+
+            Mock.Get(mockWorkspaceInfo.SavedRepoInfo)
+                .Verify(wri => wri.Create(), Times.Never);
+        }
+
+        [Fact]
+        public void SetupDotRdFolder_DotRdFolderIsHidden()
+        {
+            //Arrange
+            var hostDoc = @"C:\users\duck\MyProjects\TestExcel\TestExcel.xlsm";
+
+            var mockWorkspaceInfo
+                = WorkspaceTestsSupport.GetWorkspaceInfoFake(hostDoc);
+
+            //Act
+            WorkspaceService.SetupDotRdFolder(mockWorkspaceInfo);
+
+            //Assert
+            Assert.True(mockWorkspaceInfo
+                .DotRdInfo.Attributes.HasFlag(FileAttributes.Hidden));
+        }
+    }
+}
