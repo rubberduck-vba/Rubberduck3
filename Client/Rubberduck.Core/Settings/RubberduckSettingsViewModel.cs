0075b313c73266ea7a539a743a12df4ee2a85cf2
Mathieu Guindon <mathieu.guindon@live.ca>
2023-09-23 11:01:28 -0400

    extracted files

--- Client/Rubberduck.Core/Settings/RubberduckSettingsViewModel.cs
+++ Client/Rubberduck.Core/Settings/RubberduckSettingsViewModel.cs
@@ -1,5 +1,4 @@
 ﻿using Rubberduck.Core.WebApi;
-using Rubberduck.InternalApi.ServerPlatform;
 using Rubberduck.SettingsProvider.Model;
 using System;
 using System.Collections.Generic;
@@ -9,11 +8,6 @@ using System.Threading.Tasks;
 
 namespace Rubberduck.Core.Settings
 {
-    public interface ISettingsViewModel<TSettings>
-    {
-        TSettings ToSettings();
-    }
-
     public class RubberduckSettingsViewModel : ISettingsViewModel<RubberduckSettings>
     {
         private readonly RubberduckSettings _settings;
@@ -50,105 +44,6 @@ namespace Rubberduck.Core.Settings
                 FeatureSwitches = this.FeatureSwitches.Select(e => e.ToSettings()).ToArray(),
                 LanguageServerSettings = this.LanguageServerSettings.ToSettings(),
                 UpdateServerSettings = this.UpdateServerSettings.ToSettings(),
-            };
-        }
-    }
-
-    public class FeatureSwitchViewModel : ISettingsViewModel<FeatureSwitch>
-    {
-        private readonly FeatureSwitch _settings;
-
-        public FeatureSwitchViewModel(FeatureSwitch settings)
-        {
-            _settings = settings;
-
-            NameResourceKey = settings.NameResourceKey;
-            IsEnabled = settings.IsEnabled;
-        }
-
-        public string NameResourceKey { get; set; }
-        public bool IsEnabled { get; set; }
-
-        public FeatureSwitch ToSettings()
-        {
-            return new FeatureSwitch
-            {
-                NameResourceKey = this.NameResourceKey,
-                IsEnabled = this.IsEnabled,
-            };
-        }
-    }
-
-    public class LanguageServerSettingsViewModel : ISettingsViewModel<LanguageServerSettings>
-    {
-        private readonly LanguageServerSettings _settings;
-
-        public LanguageServerSettingsViewModel()
-            : this(LanguageServerSettings.Default) { }
-
-        public LanguageServerSettingsViewModel(LanguageServerSettings settings)
-        {
-            _settings= settings;
-
-            Path = settings.Path;
-            TransportType = settings.TransportType;
-            PipeName = settings.PipeName;
-            Mode = settings.Mode;
-            TraceLevel = settings.TraceLevel;
-        }
-
-        public string Path { get; set }
-
-        public TransportType TransportType { get; set; }
-        public string PipeName { get; set; }
-        public MessageMode Mode { get; set; }
-
-        public ServerTraceLevel TraceLevel { get; set; }
-
-        public LanguageServerSettings ToSettings()
-        {
-            return new LanguageServerSettings
-            {
-                Mode = this.Mode,
-                Path = this.Path,
-                PipeName = this.PipeName,
-                TraceLevel = this.TraceLevel,
-                TransportType = this.TransportType,
-            };
-        }
-    }
-
-    public class UpdateServerSettingsViewModel : ISettingsViewModel<UpdateServerSettings>
-    {
-        private readonly UpdateServerSettings _settings;
-
-        public UpdateServerSettingsViewModel()
-            : this(UpdateServerSettings.Default) { }
-
-        public UpdateServerSettingsViewModel(UpdateServerSettings settings)
-        {
-            _settings= settings;
-
-            CheckVersionOnStartup = settings.CheckVersionOnStartup;
-            IncludePreReleases = settings.IncludePreReleases;
-            RubberduckWebApiBaseUrl = settings.RubberduckWebApiBaseUrl;
-            Path = settings.Path;
-        }
-
-        public bool CheckVersionOnStartup { get; set; }
-        public bool IncludePreReleases { get; set; }
-        public string RubberduckWebApiBaseUrl { get; set; }
-
-        public string Path { get; set; }
-
-        public UpdateServerSettings ToSettings()
-        {
-            return new UpdateServerSettings
-            {
-                CheckVersionOnStartup = this.CheckVersionOnStartup,
-                IncludePreReleases = this.IncludePreReleases,
-                Path = this.Path,
-                RubberduckWebApiBaseUrl = this.RubberduckWebApiBaseUrl,
             };
         }
     }
