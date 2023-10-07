5dc209629ffab70360a5cbc997a781ce69cfe7eb
Mathieu Guindon <mathieu.guindon@live.ca>
2023-10-07 19:44:16 -0400

    adjusted handler namespaces

---
+++ Server/Rubberduck.LanguageServer/Handlers/Language/CompletionHandler.cs
@@ -0,0 +1,70 @@
+﻿using OmniSharp.Extensions.LanguageServer.Protocol.Client.Capabilities;
+using OmniSharp.Extensions.LanguageServer.Protocol.Document;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using OmniSharp.Extensions.LanguageServer.Protocol.Server;
+using Rubberduck.LanguageServer.Model;
+using Rubberduck.LanguageServer.Services;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Rubberduck.LanguageServer.Handlers.Language
+{
+    public class CompletionHandler : CompletionHandlerBase
+    {
+        private readonly ILanguageServerFacade _server;
+        private readonly SupportedLanguage _language;
+        private readonly DocumentContentStore _contentStore;
+
+        private readonly TextDocumentSelector _selector;
+
+        public CompletionHandler(ILanguageServerFacade server, SupportedLanguage language, DocumentContentStore contentStore)
+        {
+            _language = language;
+            _server = server;
+            _contentStore = contentStore;
+
+            var filter = new TextDocumentFilter
+            {
+                Language = language.Id,
+                Pattern = string.Join(";", language.FileTypes.Select(fileType => $"**/{fileType}").ToArray())
+            };
+            _selector = new TextDocumentSelector(filter);
+        }
+
+        public async override Task<CompletionItem> Handle(CompletionItem request, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            // TODO
+            return await Task.FromResult(request);
+        }
+
+        public async override Task<CompletionList> Handle(CompletionParams request, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            var items = new List<CompletionItem>();
+            var isIncomplete = true;
+            // TODO
+
+            var list = new CompletionList(items, isIncomplete);
+            return await Task.FromResult(list);
+        }
+
+        protected override CompletionRegistrationOptions CreateRegistrationOptions(CompletionCapability capability, ClientCapabilities clientCapabilities)
+        {
+            return new CompletionRegistrationOptions
+            {
+                DocumentSelector = _selector,
+                CompletionItem = new CompletionRegistrationCompletionItemOptions
+                {
+                    LabelDetailsSupport = true,
+                },
+                ResolveProvider = true,
+                WorkDoneProgress = true,
+                // TODO
+            };
+        }
+    }
+}
---
+++ Server/Rubberduck.LanguageServer/Handlers/Workspace/DidDeleteFileHandler.cs
@@ -0,0 +1,62 @@
+﻿using MediatR;
+using OmniSharp.Extensions.LanguageServer.Protocol.Client.Capabilities;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using OmniSharp.Extensions.LanguageServer.Protocol.Server;
+using OmniSharp.Extensions.LanguageServer.Protocol.Window;
+using OmniSharp.Extensions.LanguageServer.Protocol.Workspace;
+using Rubberduck.LanguageServer.Model;
+using Rubberduck.LanguageServer.Services;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Rubberduck.LanguageServer.Handlers.Workspace
+{
+    public class DidDeleteFileHandler : DidDeleteFileHandlerBase
+    {
+        private readonly ILanguageServerFacade _server;
+        private readonly SupportedLanguage _language;
+        private readonly DocumentContentStore _contentStore;
+
+        public DidDeleteFileHandler(ILanguageServerFacade server, SupportedLanguage language, DocumentContentStore contentStore)
+        {
+            _language = language;
+            _server = server;
+            _contentStore = contentStore;
+        }
+
+        public async override Task<Unit> Handle(DidDeleteFileParams request, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            var tasks = request.Files.Select(file =>
+            {
+                if (!_contentStore.TryRemove(file.Uri))
+                {
+                    _server.Window.LogWarning($"File '{file.Uri}' could not be removed from content store.");
+                }
+                return Task.CompletedTask;
+            });
+
+            await Task.WhenAll(tasks);
+            return Unit.Value;
+        }
+
+        protected override DidDeleteFileRegistrationOptions CreateRegistrationOptions(FileOperationsWorkspaceClientCapabilities capability, ClientCapabilities clientCapabilities)
+        {
+            return new DidDeleteFileRegistrationOptions
+            {
+                Filters = new Container<FileOperationFilter>(
+                    new FileOperationFilter
+                    {
+                        Pattern = new FileOperationPattern
+                        {
+                            Glob = string.Join(";", _language.FileTypes.Select(fileType => $"**/{fileType}").ToArray()),
+                            Matches = FileOperationPatternKind.File,
+                            Options = new FileOperationPatternOptions { IgnoreCase = true }
+                        }
+                    })
+            };
+        }
+    }
+}
