7cd3d3758dc51bb199c78fa36805d4168c331ddd
Mathieu Guindon <mathieu.guindon@live.ca>
2023-10-15 04:24:33 -0400

    more theming

---
+++ Client/Rubberduck.UI/Converters/WindowToDuckyBackgroundTranslationConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Rubberduck.UI.Converters
+{
+    public class WindowToDuckyBackgroundTranslationConverter : IValueConverter
+    {
+        public enum TranslationDirection { TranslationX, TranslationY }
+
+        public double Percent { get; set; } = 0.5;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var windowSize = (double)value;
+            return windowSize * Percent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
