4d18ca63dd6d08270b94595dcd83060451201f0c
Mathieu Guindon <mathieu.guindon@live.ca>
2023-10-18 01:37:41 -0400

    implemented systemcommands

--- Rubberduck.Editor/Shell/ShellWindow.xaml.cs
+++ Rubberduck.Editor/Shell/ShellWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using System.Windows;
+using System.Windows.Input;
 
 namespace Rubberduck.Editor.Shell
 {
@@ -11,11 +13,32 @@ namespace Rubberduck.Editor.Shell
         public ShellWindow()
         {
             InitializeComponent();
+            Handlers = new SystemCommandHandlers(this);
+
+            var systemCommands = new CommandBinding[]
+            {
+                new CommandBinding(SystemCommands.CloseWindowCommand, 
+                    Handlers.CloseWindowCommandBinding_Executed, 
+                    Handlers.CloseWindowCommandBinding_CanExecute),
+                new CommandBinding(SystemCommands.MaximizeWindowCommand, 
+                    Handlers.MaximizeWindowCommandBinding_Executed, 
+                    Handlers.MaximizeWindowCommandBinding_CanExecute),
+                new CommandBinding(SystemCommands.MinimizeWindowCommand, 
+                    Handlers.MinimizeWindowCommandBinding_Executed, 
+                    Handlers.MinimizeWindowCommandBinding_CanExecute),
+                new CommandBinding(SystemCommands.RestoreWindowCommand, 
+                    Handlers.RestoreWindowCommandBinding_Executed, 
+                    Handlers.RestoreWindowCommandBinding_CanExecute),
+            };
+
+            CommandBindings.AddRange(systemCommands);
         }
 
         private void Window_ContentRendered(object sender, EventArgs e)
         {
             InvalidateVisual();
         }
+
+        private SystemCommandHandlers Handlers { get; }
     }
 }
---
+++ Rubberduck.Editor/Shell/SystemCommandHandlers.cs
@@ -0,0 +1,63 @@
+﻿using System.Windows;
+
+namespace Rubberduck.Editor.Shell
+{
+    public class SystemCommandHandlers
+    {
+        private readonly Window _window;
+
+        public SystemCommandHandlers(Window window) 
+        {
+            _window = window;
+        }
+
+        public void CloseWindowCommandBinding_CanExecute(object sender, System.Windows.Input.CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+            e.Handled = true;
+        }
+
+        public void CloseWindowCommandBinding_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            _window.Close();
+            e.Handled = true;
+        }
+
+        public void MinimizeWindowCommandBinding_CanExecute(object sender, System.Windows.Input.CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = _window.WindowState != WindowState.Minimized;
+            e.Handled = true;
+        }
+
+        public void MinimizeWindowCommandBinding_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            _window.WindowState = WindowState.Minimized;
+            e.Handled = true;
+        }
+
+        public void MaximizeWindowCommandBinding_CanExecute(object sender, System.Windows.Input.CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = _window.WindowState != WindowState.Maximized;
+            e.Handled = true;
+        }
+
+        public void MaximizeWindowCommandBinding_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            _window.WindowState = WindowState.Maximized;
+
+            e.Handled = true;
+        }
+
+        public void RestoreWindowCommandBinding_CanExecute(object sender, System.Windows.Input.CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = _window.WindowState == WindowState.Maximized;
+            e.Handled = true;
+        }
+
+        public void RestoreWindowCommandBinding_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            _window.WindowState = WindowState.Normal;
+            e.Handled = true;
+        }
+    }
+}
