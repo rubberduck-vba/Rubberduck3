a50b99054454ab25867ad1ad052f0162a5c1ce8f
Mathieu Guindon <mathieu.guindon@live.ca>
2024-01-10 23:06:18 -0500

    add static routed commands

---
+++ Client/Rubberduck.UI/Shell/Tools/ServerTrace/ServerTraceCommands.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Input;
+
+namespace Rubberduck.UI.Shell.Tools.ServerTrace
+{
+    public static class ServerTraceCommands
+    {
+        public static RoutedCommand PauseTraceCommand { get; }
+            = new RoutedCommand(nameof(PauseTraceCommand), typeof(ServerTraceControl));
+
+        public static RoutedCommand ClearContentCommand { get; }
+            = new RoutedCommand(nameof(ClearContentCommand), typeof(ServerTraceControl));
+
+        public static RoutedCommand CopyContentCommand { get; }
+            = new RoutedCommand(nameof(CopyContentCommand), typeof(ServerTraceControl));
+
+        public static RoutedCommand OpenLogFileCommand { get; }
+            = new RoutedCommand(nameof(OpenLogFileCommand), typeof(ServerTraceControl));
+    }
+}
