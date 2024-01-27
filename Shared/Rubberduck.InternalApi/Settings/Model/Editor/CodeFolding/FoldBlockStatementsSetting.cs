fc8a472c2dd8b89cb5cd08e2c1f01248bacc95c6
Mathieu Guindon <mathieu.guindon@live.ca>
2024-01-27 04:10:21 -0500

    added code folding settings

---
+++ Shared/Rubberduck.InternalApi/Settings/Model/Editor/CodeFolding/FoldBlockStatementsSetting.cs
@@ -0,0 +1,11 @@
+﻿namespace Rubberduck.InternalApi.Settings.Model.Editor.CodeFolding;
+
+public record class FoldBlockStatementsSetting : BooleanRubberduckSetting
+{
+    public static bool DefaultSettingValue { get; } = true;
+
+    public FoldBlockStatementsSetting()
+    {
+        DefaultValue = DefaultSettingValue;
+    }
+}
---
+++ Shared/Rubberduck.InternalApi/Settings/Model/Editor/CodeFolding/FoldModuleHeaderSetting.cs
@@ -0,0 +1,11 @@
+﻿namespace Rubberduck.InternalApi.Settings.Model.Editor.CodeFolding;
+
+public record class FoldModuleHeaderSetting : BooleanRubberduckSetting
+{
+    public static bool DefaultSettingValue { get; } = true;
+
+    public FoldModuleHeaderSetting()
+    {
+        DefaultValue = DefaultSettingValue;
+    }
+}
