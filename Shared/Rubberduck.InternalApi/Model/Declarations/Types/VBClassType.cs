df2b94cb3b232b85222596beb75874ec0bfcd9b3
Mathieu Guindon <mathieu.guindon@live.ca>
2024-01-27 18:07:41 -0500

    extracted class from file

--- Shared/Rubberduck.InternalApi/Model/Declarations/Types/VBClassType.cs
+++ Shared/Rubberduck.InternalApi/Model/Declarations/Types/VBClassType.cs
@@ -20,18 +20,3 @@ public record class VBClassType : VBMemberOwnerType
     public override VBType[] ConvertsSafelyToTypes => Supertypes.Concat([VbVariantType, VbObjectType]).ToArray();
     public override object? DefaultValue { get; } = VBObjectType.Nothing;
 }
-
-/// <summary>
-/// An object type that can be iterated in a <c>For Each...Next</c> loop.
-/// </summary>
-public record class VBCollectionType : VBClassType, IEnumerableType
-{
-    public VBCollectionType(string name, Uri uri, bool isUserDefined = false, IEnumerable<VBTypeMember>? members = null, VBReturningMember? newEnumMember = null) 
-        : base(name, uri, isUserDefined, members)
-    {
-        NewEnumMember = newEnumMember;
-    }
-
-    public bool IsArray { get; } = false;
-    public VBReturningMember? NewEnumMember { get; init; }
-}
---
+++ Shared/Rubberduck.InternalApi/Model/Declarations/Types/VBCollectionType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Rubberduck.InternalApi.Model.Declarations.Types.Abstract;
+
+namespace Rubberduck.InternalApi.Model.Declarations.Types;
+
+/// <summary>
+/// An object type that can be iterated in a <c>For Each...Next</c> loop.
+/// </summary>
+public record class VBCollectionType : VBClassType, IEnumerableType
+{
+    public VBCollectionType(string name, Uri uri, bool isUserDefined = false, IEnumerable<VBTypeMember>? members = null, VBReturningMember? newEnumMember = null) 
+        : base(name, uri, isUserDefined, members)
+    {
+        NewEnumMember = newEnumMember;
+    }
+
+    public bool IsArray { get; } = false;
+    public VBReturningMember? NewEnumMember { get; init; }
+}
