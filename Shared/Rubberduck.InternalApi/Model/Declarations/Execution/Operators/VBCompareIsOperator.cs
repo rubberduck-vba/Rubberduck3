924b4404b59f6803fb603c2f4b0d49570d101901
Mathieu Guindon <mathieu.guindon@live.ca>
2024-02-04 20:55:33 -0500

    implemented 'Is' compare operator

--- Shared/Rubberduck.InternalApi/Model/Declarations/Execution/Operators/VBCompareIsOperator.cs
+++ Shared/Rubberduck.InternalApi/Model/Declarations/Execution/Operators/VBCompareIsOperator.cs
@@ -2,6 +2,7 @@
 using Rubberduck.InternalApi.Model.Declarations.Execution.Values;
 using Rubberduck.InternalApi.Model.Declarations.Operators.Abstract;
 using Rubberduck.InternalApi.Model.Declarations.Symbols;
+using Rubberduck.InternalApi.Model.Declarations.Types.Abstract;
 using System;
 
 namespace Rubberduck.InternalApi.Model.Declarations.Operators;
@@ -15,6 +16,18 @@ public record class VBCompareIsOperator : VBComparisonOperator
 
     protected override VBTypedValue ExecuteBinaryOperator(ref VBExecutionScope context, VBTypedValue lhsValue, VBTypedValue rhsValue)
     {
-        throw new NotImplementedException();
+        if (VBType.IntrinsicTypes.Contains(lhsValue.TypeInfo)
+            || lhsValue is not VBObjectValue lhsObj)
+        {
+            throw VBRuntimeErrorException.TypeMismatch(lhsValue.Symbol!);
+        }
+        
+        if (VBType.IntrinsicTypes.Contains(rhsValue.TypeInfo)
+            || rhsValue is not VBObjectValue rhsObj)
+        {
+            throw VBRuntimeErrorException.TypeMismatch(rhsValue.Symbol!);
+        }
+
+        return new VBBooleanValue(this) { Value = ReferenceEquals(lhsObj, rhsObj) };
     }
 }
