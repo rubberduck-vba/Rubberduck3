babb3ab3415b95dfab11fb20d3b0b6674f79bbe6
Mathieu Guindon <mathieu.guindon@live.ca>
2024-02-20 00:41:46 -0500

    added syntax error model

---
+++ Server/Rubberduck.Parsing/Exceptions/AntlrSyntaxErrorInfo.cs
@@ -0,0 +1,31 @@
+using Antlr4.Runtime;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using Rubberduck.InternalApi.Extensions;
+using Rubberduck.Parsing.Model;
+
+namespace Rubberduck.Parsing.Exceptions;
+
+public record class AntlrSyntaxErrorInfo
+{
+    public WorkspaceFileUri Uri { get; init; } = default!;
+    public CodeKind CodeKind { get; init; }
+
+    public string Message { get; init; } = default!;
+
+    public RecognitionException Exception { get; init; } = default!;
+    public IToken OffendingSymbol { get; init; } = default!;
+
+    public int LineNumber { get; init; }
+    public int Position { get; init; }
+
+    public OmniSharp.Extensions.LanguageServer.Protocol.Models.Range Range() =>
+        new(start: new Position(OffendingSymbol.Line, OffendingSymbol.Column), 
+            end: new Position(OffendingSymbol.EndLine(), OffendingSymbol.EndColumn()));
+
+    public SyntaxErrorInfo ToSyntaxErrorInfo() => new() 
+    {
+        Uri = Uri,
+        Message = Message,
+        Range = Range(),
+    };
+}
---
+++ Shared/Rubberduck.InternalApi/Model/SyntaxErrorInfo.cs
@@ -0,0 +1,11 @@
+﻿using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using Rubberduck.InternalApi.Extensions;
+
+namespace Rubberduck.Parsing.Model;
+
+public record class SyntaxErrorInfo
+{
+    public WorkspaceFileUri Uri { get; init; } = default!;
+    public Range Range { get; init; } = default!;
+    public string Message { get; init; } = default!;
+}
